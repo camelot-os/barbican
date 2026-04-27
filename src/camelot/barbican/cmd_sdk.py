a85a99c5a8de84ed3806b9a617e7ff2a53548d08
Florent Valette <florent.valette@gmail.com>
2026-04-27 09:51:50 +0200

    barbican: add empty sdk command

---
+++ src/camelot/barbican/cmd_sdk.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2026 H2Lab
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from argparse import ArgumentParser, Namespace
+
+
+def add_arguments(parser: ArgumentParser) -> None: ...
+
+
+def run(args: Namespace) -> None: ...
